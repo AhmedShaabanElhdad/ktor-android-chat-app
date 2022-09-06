3ccb90b7d9a6f88b04b23ce4b039a6a8075b113b
Ahmed Shaban <ahmed.shaaban.elhdah@gmail.com>
2022-09-06 10:13:14 +0300

    structure the app
    - make structure for the app
    - add dependency
    - make app class
    
    #1

---
+++ app/src/main/java/com/example/ktorchatingapp/presentation/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.ktorchatingapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgs
+import androidx.navigation.navArgument
+import com.example.ktorchatingapp.presentation.chat.ChatScreen
+import com.example.ktorchatingapp.presentation.login.LoginScreen
+import com.example.ktorchatingapp.presentation.theme.KtorChatingAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KtorChatingAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KtorChatingAppTheme {
+
+    }
+}
